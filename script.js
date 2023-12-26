548ae3e2a59ea5013d5c7022a4f2c5355ddfc2f1
fahryanputra <fahryanputra@gmail.com>
2023-12-26 11:58:38 +0700

    Add red border for not matching password input

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const confirmPassword = document.querySelector(".password-confirmation");
 const submitButton = document.getElementById("submit-button");
 
 submitButton.addEventListener("click", (event) => {
-    const passwordInput = document.getElementById("password").value;
-    const confirmationInput = document.getElementById("confirm-password").value;
+    const passwordInput = document.getElementById("password");
+    const confirmationInput = document.getElementById("confirm-password");
 
-    if(passwordInput !== confirmationInput) {
+    if(passwordInput.value !== confirmationInput.value) {
         event.preventDefault();
-        confirmPassword.textContent = "The password do not match!";
+        passwordInput.setAttribute("class", "error");
+        confirmationInput.setAttribute("class", "error");
+        confirmPassword.textContent = "*The password do not match";
         return false;
     }
 })
